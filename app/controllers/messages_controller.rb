df5f4616951986733c62d6f1f6d0a8122bd0e1f0
soko <sfujino@netprotections.co.jp>
2016-06-17 01:50:16 +0000

    はい

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     # Messageを全て取得する。
     @messages = Message.all
   end
   
-
-def create
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -16,11 +29,19 @@ def create
       flash.now[:alert] = "メッセージの保存に失敗しました。"
       render 'index'
     end
-end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+    def set_message
+    @message = Message.find(params[:id])
   end
 end
