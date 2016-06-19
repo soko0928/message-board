1ed39380a1211ea3316cf13da0a165b782af55f2
soko <sfujino@netprotections.co.jp>
2016-06-19 11:59:30 +0000

    hai

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
     #名前は必須入力かつ20字以内
     validates :name , length: { maximum: 20} , presence: true
+    validates :age , length: { minimum: 2, maximum: 20} , numericality: 0:greater_than_or_equal_totrue
     #内容は必須入力かつ2時以上30文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
